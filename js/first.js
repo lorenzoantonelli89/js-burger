6b1489408cc39e0757116a7cb71084d40cfa0118
Lorenzo <lorenzo.antonelli89@gmail.com>
2021-03-11 15:10:37 +0100

    update

--- js/first.js
+++ js/first.js
@@ -2,12 +2,34 @@
 var calculateBtn = document.getElementById('calculate');
 
 calculateBtn.addEventListener('click', function(){
-  var checkboxs = document.getElementsByClassName('ingredient');
-    for (var i = 0; i < checkboxs.length; i++) {
-      var checkboxElem = checkboxs[i]
-      var priceElem = parseInt(checkboxElem.dataset.value);
-      console.log(checkboxElem, priceElem);
-    }
+
+  var burgerName = document.getElementById('burgername').value;
+
+  if (burgerName.length < 1) {
+
+    alert('Insert hamburger name');
+
+  }else {
+    var totalPrice = 6;
+    var checkboxs = document.getElementsByClassName('ingredient');
+
+      for (var i = 0; i < checkboxs.length; i++) {
+
+        var checkboxElem = checkboxs[i]
+        var flag = checkboxElem.checked;
+        var priceElem = parseInt(checkboxElem.dataset.price);
+
+        if (flag) {
+
+          totalPrice += priceElem;
+        }
+
+      }
+      console.log(priceElem);
+  }
+
+
+
 
 
 })
